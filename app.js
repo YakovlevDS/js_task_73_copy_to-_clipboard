c739f33ce77b43020a9305df37c05eff3dcfd796
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-23 18:56:28 +0200

    added js copy to clipboard

---
+++ app.js
@@ -0,0 +1,10 @@
+// ? Task:Copy to Clipboard
+
+
+// Solution 1
+const copyToClipboard = text => navigator.clipboard.writeText(text);
+
+copyToClipboard("Hello World");
+
+
+// ! Explanation: Easily copy any text to clipboard using navigator.clipboard.writeText.
